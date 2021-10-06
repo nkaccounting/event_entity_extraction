b99da03f605576318c3a0cd0f83a735d6c8875f3
nkaccouting <759893882@qq.com>
2021-10-06 14:56:23 +0800

    把生成json文件的方法抽象成函数，3种数据集都可以复用

--- ccks/create_squad_json.py
+++ ccks/create_squad_json.py
@@ -1,66 +1,20 @@
 import json
+import os.path
 
 import pandas as pd
 
-df = pd.read_csv('train.csv', header=None, index_col=None, encoding='utf-8')
-df = df.fillna("#")
 
-data = []
-for i in df.itertuples():
-    id = str(i[1])
-    context = i[2]
-    question = i[3]
-    answers = i[4]
+def creat_json(df: pd.DataFrame, type: str):
+    data = []
+    for i in df.itertuples():
+        id = str(i[1])
+        context = i[2]
+        question = i[3]
+        answers = i[4]
 
-    is_impossible = False
-    # 没有答案的情况
-    if answers == "#":
-        is_impossible = True
-        answers = []
-        result = {
-            "title": id,
-            'paragraphs': [
-                {
-                    'qas': [
-                        {
-                            'question': question,
-                            'id': id,
-                            'answers': answers,
-                            'plausible_answers': [],
-                            'is_impossible': is_impossible
-                        }
-                    ],
-                    'context': context
-                }
-            ]
-        }
-    else:
-        try:
-            answer_start = context.index(answers)
-            answers = [
-                {
-                    "text": answers,
-                    'answer_start': answer_start
-                }
-            ]
-            result = {
-                "title": id,
-                'paragraphs': [
-                    {
-                        'qas': [
-                            {
-                                'question': question,
-                                'id': id,
-                                'answers': answers,
-                                'is_impossible': is_impossible
-                            }
-                        ],
-                        'context': context
-                    }
-                ]
-            }
-        except:
-            print('no answer！')
+        is_impossible = False
+        # 没有答案的情况
+        if answers == "#":
             is_impossible = True
             answers = []
             result = {
@@ -80,10 +34,59 @@ for i in df.itertuples():
                     }
                 ]
             }
-    data.append(result)
+        else:
+            try:
+                answer_start = context.index(answers)
+                answers = [
+                    {
+                        "text": answers,
+                        'answer_start': answer_start
+                    }
+                ]
+                result = {
+                    "title": id,
+                    'paragraphs': [
+                        {
+                            'qas': [
+                                {
+                                    'question': question,
+                                    'id': id,
+                                    'answers': answers,
+                                    'is_impossible': is_impossible
+                                }
+                            ],
+                            'context': context
+                        }
+                    ]
+                }
+            except:
+                print('no answer！')
+                is_impossible = True
+                answers = []
+                result = {
+                    "title": id,
+                    'paragraphs': [
+                        {
+                            'qas': [
+                                {
+                                    'question': question,
+                                    'id': id,
+                                    'answers': answers,
+                                    'plausible_answers': [],
+                                    'is_impossible': is_impossible
+                                }
+                            ],
+                            'context': context
+                        }
+                    ]
+                }
+        data.append(result)
 
-with open(('VU_squad2.0_train.json'), 'w', encoding='utf-8') as fp:
-    json.dump({
-        'version': 'v2.0',
-        'data': data
-    }, fp, ensure_ascii=False, indent=2)
+    if not os.path.exists('./json'):
+        os.makedirs('./json')
+
+    with open(('./json/VU_squad2.0_{type}.json').format(type=type), 'w', encoding='utf-8') as fp:
+        json.dump({
+            'version': 'v2.0',
+            'data': data
+        }, fp, ensure_ascii=False, indent=2)
