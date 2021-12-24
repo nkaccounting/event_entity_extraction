5ed1b1cef8823ec1e05a712fd9cd48e6d6e11b7c
nkaccounting <759893882@qq.com>
2021-12-24 14:25:53 +0800

    hugging face init

---
+++ hugging_face_QA/train.sh
@@ -0,0 +1,13 @@
+python3.8 run_seq2seq_qa.py \
+  --model_name_or_path t5-small \
+  --train_file VU_squad2.0_hug.json\
+  --context_column context \
+  --question_column question \
+  --answer_column answers \
+  --do_train \
+  --per_device_train_batch_size 12 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 2 \
+  --max_seq_length 384 \
+  --doc_stride 128 \
+  --output_dir ./tmp/debug_seq2seq_squad/
