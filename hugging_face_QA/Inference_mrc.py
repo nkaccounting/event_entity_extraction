c974c60cde349b6481b44c1ca9c8fd8638c65bc8
nkaccounting <759893882@qq.com>
2021-12-27 16:21:34 +0800

    修改最新版的prompt question形式

--- hugging_face_QA/Inference_mrc.py
+++ hugging_face_QA/Inference_mrc.py
@@ -12,6 +12,9 @@ pipeline = QuestionAnsweringPipeline(model=model, tokenizer=tokenizers)
 question = ['信批违规', '实控人股东变更', '交易违规', '涉嫌非法集资', '不能履职', '重组失败', '评级调整', '业绩下滑', '涉嫌违法', '财务造假', '涉嫌传销', '涉嫌欺诈',
             '资金账户风险', '高管负面',
             '资产负面', '投诉维权', '产品违规', '提现困难', '失联跑路', '歇业停业', '公司股市异常']
+
+question = ['发生' + q + '的是？' for q in question]
+
 context = '今年6月，经检察机关批准，广州警方以涉嫌组织、领导传销活动罪对云联惠公司实际控制人黄某等主要犯罪嫌疑人执行逮捕,据悉，万事达有限责任公司严重违法，财务数据造假明显，高层领导人无法履行职责，此公司已被暂停营业'
 
 t1 = time.time()
@@ -24,4 +27,4 @@ t2 = time.time()
 for i, r in enumerate(res):
     print(question[i], r)
 
-print(t2-t1)
+print(t2 - t1)
